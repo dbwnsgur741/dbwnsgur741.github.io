64f2c6a288eb98d2ad24dffb54fc35777d2fd836
Drop D Tuning <dbwnsgur741@naver.com>
2021-12-09 23:34:46 +0900

    Install bundle & Jekyll

---
+++ _posts/2011-12-31-new-years-eve-is-awesome.md
@@ -0,0 +1,8 @@
+---
+layout: archive
+title:  "첫번째 게시글"
+---
+
+# Welcome
+
+**Hello world**
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "첫번째 게시글",
+        "excerpt":"Welcome   Hello world  ","categories": [],
+        "tags": [],
+        "url": "/new-years-eve-is-awesome/",
+        "teaser": null
+      }]
