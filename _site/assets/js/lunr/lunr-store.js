a4b6740bfb52bbef430a2d11330e3552ef88d530
Drop D Tuning <dbwnsgur741@naver.com>
2021-12-28 22:36:03 +0900

    modify abstract factory

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -34,4 +34,10 @@ var store = [{
         "tags": ["Redux"],
         "url": "/redux/ReactNative-Redux/",
         "teaser": null
+      },{
+        "title": "[Design Pattern] - Abstract Factory pattern",
+        "excerpt":"추상 팩토리 패턴(Abstract Factory pattern)     추상 팩토리 패턴이란?      객체지향 디자인 패턴이다.   추상 팩토리 패턴은 상세화된 서브 클래스를 정의하지 않고도 서로 관련성이 있거나 독립적인 여러 객체의 군을 생성하기 위한 인터페이스를 제공한다.    ","categories": ["Design Pattern"],
+        "tags": ["Design Pattern"],
+        "url": "/design%20pattern/DesignPattern-AbstractFactory/",
+        "teaser": null
       }]
